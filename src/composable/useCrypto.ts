7a33b4014349935e503e2673e1133cd21b150929
¨José <josegomesd6@gmail.com>
2025-03-01 18:07:19 -0300

    fix: prettier

--- src/composable/useCrypto.ts
+++ src/composable/useCrypto.ts
@@ -5,61 +5,53 @@ export interface CryptoPrices {
   cardano: { usd: number; changePercent: number };
 }
 
-export const getCryptoPrices =
-  async (): Promise<CryptoPrices | null> => {
-    try {
-      const response = await fetch(
-        'https://api.binance.com/api/v3/ticker/24hr?symbols=["BTCBRL","ETHBRL","SOLBRL","ADABRL"]'
-      );
-      const data = await response.json();
+export const getCryptoPrices = async (): Promise<CryptoPrices | null> => {
+  try {
+    const response = await fetch(
+      'https://api.binance.com/api/v3/ticker/24hr?symbols=["BTCBRL","ETHBRL","SOLBRL","ADABRL"]'
+    );
+    const data = await response.json();
 
-      return {
-        bitcoin: {
-          usd: parseFloat(
-            data.find((item: any) => item.symbol === 'BTCBRL')
-              ?.lastPrice || '0'
-          ),
-          changePercent: parseFloat(
-            data.find((item: any) => item.symbol === 'BTCBRL')
-              ?.priceChangePercent || '0'
-          ),
-        },
-        ethereum: {
-          usd: parseFloat(
-            data.find((item: any) => item.symbol === 'ETHBRL')
-              ?.lastPrice || '0'
-          ),
-          changePercent: parseFloat(
-            data.find((item: any) => item.symbol === 'ETHBRL')
-              ?.priceChangePercent || '0'
-          ),
-        },
-        solana: {
-          usd: parseFloat(
-            data.find((item: any) => item.symbol === 'SOLBRL')
-              ?.lastPrice || '0'
-          ),
-          changePercent: parseFloat(
-            data.find((item: any) => item.symbol === 'SOLBRL')
-              ?.priceChangePercent || '0'
-          ),
-        },
-        cardano: {
-          usd: parseFloat(
-            data.find((item: any) => item.symbol === 'ADABRL')
-              ?.lastPrice || '0'
-          ),
-          changePercent: parseFloat(
-            data.find((item: any) => item.symbol === 'ADABRL')
-              ?.priceChangePercent || '0'
-          ),
-        },
-      };
-    } catch (error) {
-      console.error(
-        'Erro ao buscar os preços das criptomoedas',
-        error
-      );
-      return null;
-    }
-  };
+    return {
+      bitcoin: {
+        usd: parseFloat(
+          data.find((item: any) => item.symbol === 'BTCBRL')?.lastPrice || '0'
+        ),
+        changePercent: parseFloat(
+          data.find((item: any) => item.symbol === 'BTCBRL')
+            ?.priceChangePercent || '0'
+        ),
+      },
+      ethereum: {
+        usd: parseFloat(
+          data.find((item: any) => item.symbol === 'ETHBRL')?.lastPrice || '0'
+        ),
+        changePercent: parseFloat(
+          data.find((item: any) => item.symbol === 'ETHBRL')
+            ?.priceChangePercent || '0'
+        ),
+      },
+      solana: {
+        usd: parseFloat(
+          data.find((item: any) => item.symbol === 'SOLBRL')?.lastPrice || '0'
+        ),
+        changePercent: parseFloat(
+          data.find((item: any) => item.symbol === 'SOLBRL')
+            ?.priceChangePercent || '0'
+        ),
+      },
+      cardano: {
+        usd: parseFloat(
+          data.find((item: any) => item.symbol === 'ADABRL')?.lastPrice || '0'
+        ),
+        changePercent: parseFloat(
+          data.find((item: any) => item.symbol === 'ADABRL')
+            ?.priceChangePercent || '0'
+        ),
+      },
+    };
+  } catch (error) {
+    console.error('Erro ao buscar os preços das criptomoedas', error);
+    return null;
+  }
+};
--- src/types/stock.ts
+++ src/types/stock.ts
@@ -5,3 +5,20 @@ export interface Stock {
   stockValuation: number;
   img?: string;
 }
+
+export type tagInvestment =
+  | 'ETF'
+  | 'STOCKS'
+  | 'CRYPTO'
+  | 'FIXED INCOME'
+  | 'REAL STATE';
+
+export interface Investment {
+  id: string;
+  name: string;
+  tag: tagInvestment;
+  value: number;
+  priceBuy: number;
+  priceCurrent: number;
+  porcentageLast24Hours: number;
+}
